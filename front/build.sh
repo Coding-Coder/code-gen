78f2e4b0489f49bcd231a8078f1d1f1c4befb3a9
tanghc <thc8775@163.com>
2020-09-07 09:05:33 +0800

    v1.3.0

--- front/build.sh
+++ front/build.sh
@@ -4,7 +4,7 @@ server_dest="../gen/src/main/resources/public"
 
 rm -rf dist/*
 npm run build:prod
-echo "复制dist文件内容到gen/src/main/resources/public"
+echo "复制dist文件内容到$server_dest"
 rm -rf $server_dest
 mkdir -p $server_dest
 cp -r dist/* $server_dest
