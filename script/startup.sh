6aaeb6318f5f6bafd89e42cf8c9d628465348c5d
tanghc <thc8775@163.com>
2020-10-26 10:08:29 +0800

    v1.3.2

--- script/startup.sh
+++ script/startup.sh
@@ -3,4 +3,4 @@
 # 先关闭服务
 sh shutdown.sh
 # --server.port：启动端口
-nohup java -jar -Xms128m -Xmx128m gen.jar --server.port=6969 &
+nohup java -jar -Xms128m -Xmx128m gen.jar --server.port=6969 >/dev/null 2>&1 &
