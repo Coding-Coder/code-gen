4f4bdfd5e47b116fb05b93d4eb90e109d8999c68
lxy <aethon47@163.com>
2021-07-09 13:35:25 +0800

    feat:修复生成表字段后顺序不对的情况

--- gen/src/main/java/com/gitee/gen/gen/mysql/MySqlColumnSelector.java
+++ gen/src/main/java/com/gitee/gen/gen/mysql/MySqlColumnSelector.java
@@ -32,7 +32,8 @@ public class MySqlColumnSelector extends ColumnSelector {
             " EXTRA AS 'Extra', " +
             " COLUMN_COMMENT AS 'Comment' " +
             " FROM information_schema.`COLUMNS` " +
-            " WHERE 1=1 AND TABLE_SCHEMA = '%s' AND TABLE_NAME = '%s' ";
+            " WHERE 1=1 AND TABLE_SCHEMA = '%s' AND TABLE_NAME = '%s' " +
+            " ORDER BY ORDINAL_POSITION ";
 
     public MySqlColumnSelector(GeneratorConfig generatorConfig) {
         super(generatorConfig);
